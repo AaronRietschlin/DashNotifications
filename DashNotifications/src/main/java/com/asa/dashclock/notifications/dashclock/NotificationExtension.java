c33304a0d8ea98767a2e63cc1bbd39ee6e232f7c
Aaron Rietschlin <nemisis.82@gmail.com>
2013-07-25 21:24:33 -0400

    Added dummy extension data. Will remove later.

--- DashNotifications/src/main/java/com/asa/dashclock/notifications/dashclock/NotificationExtension.java
+++ DashNotifications/src/main/java/com/asa/dashclock/notifications/dashclock/NotificationExtension.java
@@ -1,15 +1,24 @@
 package com.asa.dashclock.notifications.dashclock;
 
+import android.content.Intent;
+
+import com.asa.dashclock.notifications.MainActivity;
 import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
 
 /**
  * Created by Aaron on 7/25/13.
  */
-public class NotificationExtension extends DashClockExtension{
+public class NotificationExtension extends DashClockExtension {
     @Override
     protected void onUpdateData(int i) {
         // TODO Get the notifications.
         // TODO Create #ExtensionData object
         // TODO Call #publishUpdate with the ExtensionData
+
+        // TODO This is test data
+        ExtensionData data = new ExtensionData();
+        data.clickIntent(new Intent(getApplicationContext(), MainActivity.class)).contentDescription("Test Data for now.").expandedBody("Expanded Body content.").expandedTitle("Expanded title");
+        publishUpdate(data);
     }
 }
